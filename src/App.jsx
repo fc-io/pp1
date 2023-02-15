970ea583415e5c76297b9ececa4124e1fca15e1a
Fredrik Carlsson <me@fc.io>
2023-02-15 13:02:42 +0100

    add something

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         start: new Date(),
         end: midsummer
       })
+
       setUntilMidsummer(interval)
     }, 1000)
 
@@ -28,7 +29,7 @@ function App() {
           <h1 className="days">{untilMidsummer.months} months<br/>
             {untilMidsummer.days} days<br />
             {untilMidsummer.hours}:{untilMidsummer.minutes}:{untilMidsummer.seconds}</h1>
-          {/* <h2 className="time">and 11h, 12m, 30s</h2> */}
+          <h2 className="time">{count}</h2>
         </div>
         <div className="rightContainer"></div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 // import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+// setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+// }, 100)
