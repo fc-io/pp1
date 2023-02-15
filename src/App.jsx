833f8e3c8d1c8067bbc6cb69e6f0c9b0608ebd37
Fredrik Carlsson <me@fc.io>
2023-02-15 10:24:17 +0100

    add until function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { parseISO, intervalToDuration } from 'date-fns'
 // import reactLogo from './assets/react.svg'
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [untilMidsummer, setUntilMidsummer] = useState({})
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const midsummer = parseISO('2023-06-24T00:00:00')
+      const interval = intervalToDuration({
+        start: new Date(),
+        end: midsummer
+      })
+      setUntilMidsummer(interval)
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
 
   return (
     <div className="App">
       <div className="outerContainer">
         <div className="leftContainer">
-          <h1 className="days">3D<br/>
-            models of abstract
-            digital art</h1>
+          <h1 className="days">{untilMidsummer.months} months<br/>
+            {untilMidsummer.days} days<br />
+            {untilMidsummer.hours}:{untilMidsummer.minutes}:{untilMidsummer.seconds}</h1>
           {/* <h2 className="time">and 11h, 12m, 30s</h2> */}
         </div>
         <div className="rightContainer"></div>
