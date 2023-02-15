844bf1e8580c3d4c37ed2ac9d1532a10c33dbe29
Fredrik Carlsson <me@fc.io>
2023-02-15 13:19:09 +0100

    add better useInterval

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,43 @@ import { useEffect, useState } from "react";
 import { parseISO, intervalToDuration } from 'date-fns'
 // import reactLogo from './assets/react.svg'
 import "./App.css";
+import useInterval from 'use-interval'
+
+function getInterval () {
+  const midsummer = parseISO('2023-06-24T00:00:00')
+  const interval = intervalToDuration({
+    start: new Date(),
+    end: midsummer
+  })
+
+  return interval
+}
 
 function App() {
-  const [untilMidsummer, setUntilMidsummer] = useState({})
+  const [untilMidsummer, setUntilMidsummer] = useState(getInterval())
+  const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      const midsummer = parseISO('2023-06-24T00:00:00')
-      const interval = intervalToDuration({
-        start: new Date(),
-        end: midsummer
-      })
+  useInterval(() => {
+    const interval = getInterval()
+    setUntilMidsummer(interval)
+    setCount(count + 1);
+  }, 1000);
 
-      setUntilMidsummer(interval)
-    }, 1000)
 
-    return () => {
-      clearInterval(intervalId)
-    }
-  }, [])
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     const interval = getInterval()
+
+  //     console.log('setInterval')
+
+  //     setUntilMidsummer(interval)
+  //     setCount((latestCount) => latestCount + 1)
+  //   }, 1000)
+
+  //   return () => {
+  //     clearInterval(intervalId)
+  //   }
+  // }, [])
 
   return (
     <div className="App">
@@ -29,7 +47,7 @@ function App() {
           <h1 className="days">{untilMidsummer.months} months<br/>
             {untilMidsummer.days} days<br />
             {untilMidsummer.hours}:{untilMidsummer.minutes}:{untilMidsummer.seconds}</h1>
-          <h2 className="time">{count}</h2>
+          <h2 className="time">count: {count}</h2>
         </div>
         <div className="rightContainer"></div>
       </div>
